1877600484e90df6b214460d84a31c2b2f3456f3
lemaldwin <lemuelaldwingarcia@gmail.com>
2025-08-29 19:56:38 +0800

    Modify ItemRepository to extend JpaRepository instead of CrudRepository

--- src/main/java/rslack/tracker/repository/ItemRepository.java
+++ src/main/java/rslack/tracker/repository/ItemRepository.java
@@ -1,9 +1,11 @@
 package rslack.tracker.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rslack.tracker.entity.ItemEntity;
 
+import java.util.UUID;
+
 @Repository
-public interface ItemRepository extends CrudRepository<ItemEntity, String> {
+public interface ItemRepository extends JpaRepository<ItemEntity, UUID> {
 }
